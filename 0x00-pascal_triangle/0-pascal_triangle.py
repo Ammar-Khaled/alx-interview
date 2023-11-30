4436e48d3a607a825a6cf18afaacdeb7fc6b93c5
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-11-30 15:53:21 +0000

    Use another approach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Define a method that generates Pascal's Triangle."""
-from math import factorial
 
 
 def pascal_triangle(n):
@@ -8,14 +7,19 @@ def pascal_triangle(n):
     Return a pascal triangle of n.
 
     Return a list of lists of integers representing
-    the Pascal's triangle of n in time O(n).
+    the Pascal's triangle of n in time O(n^2).
     """
     pascal_triangle = []
-    for i in range(n):
+    for i in range(1, n + 1):
         row = []
-        for j in range(i + 1):
-            # iCj = i!/((i-j)!*j!)
-            row.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        # first value in a line is always 1
+        C = 1
+        for j in range(1, i + 1):
+            row.append(C)
+
+            # using Binomial Coefficient
+            C = C * (i - j) // j
+
         pascal_triangle.append(row)
 
     return pascal_triangle
