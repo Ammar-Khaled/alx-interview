7552040f61e8998fad64e56ffea51b89b9e284c8
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-16 12:43:52 +0000

    Fix: semistandard codestyle

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 const request = require("request");
-movie_id = process.argv[2]
+const movieId = process.argv[2];
 request(
-  "https://swapi-api.alx-tools.com/api/films/" + movie_id + "/",
-  async function (_, _, body) {
-    characters = JSON.parse(body).characters;
-    for (char of characters) {
+  "https://swapi-api.alx-tools.com/api/films/" + movieId + "/",
+  async function (error, response, body) {
+    if (error) return console.error(error);
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
       await new Promise((resolve, reject) => {
-        request(char, function (err, res, content) {
+        request(character, function (err, res, content) {
+          if (err) return console.error(err);
           console.log(JSON.parse(content).name);
           resolve();
-        })
-      })
+        });
+      });
     }
   }
 );
